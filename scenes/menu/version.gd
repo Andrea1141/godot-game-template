63e944b612885d2d2418ff1625e1967584a0ef2f
Davide <davcri91@gmail.com>
2021-04-02 15:53:08 +0200

    Add tmp version debug scene as autoload

--- scenes/menu/version.gd
+++ scenes/menu/version.gd
@@ -1,4 +1,13 @@
 extends Label
 
 func _ready():
+	visible = false
+	if not OS.is_debug_build():
+		queue_free()
 	text = ProjectSettings.get_setting("application/config/version")
+	rect_position.y = Game.size.y - 50
+	rect_position.x = 20
+
+func _process(delta):
+	if Input.is_action_just_pressed("debug_show"):
+		visible = !visible
